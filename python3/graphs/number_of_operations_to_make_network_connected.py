a37303d171824a7638fc00cf25e75d60fd2202a5
Keerthivasan Raghavan <mail2akash97@gmail.com>
2025-12-18 06:23:35 +0000

    06:23:35 +0000 Thu, 18-Dec-2025

---
+++ python3/graphs/number_of_operations_to_make_network_connected.py
@@ -0,0 +1,22 @@
+# link: https://leetcode.com/problems/number-of-operations-to-make-network-connected/description/?envType=problem-list-v2&envId=graph
+
+
+from typing import List
+
+
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        pass
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_case0(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
