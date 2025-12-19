4242aa99a3f6efa7f05bd254a6298e6c4416876a
Keerthivasan Raghavan <mail2akash97@gmail.com>
2025-12-19 06:16:52 +0000

    06:16:52 +0000 Fri, 19-Dec-2025

---
+++ python3/graphs/number_of_ways_to_arrive_at_destination.py
@@ -0,0 +1,24 @@
+# link: https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/description/?envType=problem-list-v2&envId=graph
+
+from typing import List
+
+
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        pass
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_case_0(self):
+        pass
+
+    def test_case_1(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
