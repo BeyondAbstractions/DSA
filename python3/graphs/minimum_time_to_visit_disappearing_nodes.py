807760a604fc6f87b5a9cfea221e210e9dfc8653
Keerthivasan Raghavan <mail2akash97@gmail.com>
2025-12-22 10:54:51 +0000

    10:54:51 +0000 Mon, 22-Dec-2025

---
+++ python3/graphs/minimum_time_to_visit_disappearing_nodes.py
@@ -0,0 +1,23 @@
+# link: https://leetcode.com/problems/find-eventual-safe-states/description/?envType=problem-list-v2&envId=graph
+
+from typing import List
+
+
+class Solution:
+    def minimumTime(
+        self, n: int, edges: List[List[int]], disappear: List[int]
+    ) -> List[int]:
+        pass
+
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_case_0(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
